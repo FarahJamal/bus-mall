436d7b9d45d7665aaa71e921b3e90a94ae1beaf4
FarahJamal <farahsarese@gmail.com>
2021-06-08 03:09:29 +0300

    adding charts

--- js/app.js
+++ js/app.js
@@ -6,14 +6,128 @@ let centerImgEl=document.getElementById('center-img');
 let rightImgEl=document.getElementById('right-img');
 let submitButton=document.getElementById('subB');
 let selectedValue = document.getElementById("list");
+let prevButton = document.getElementById('prev-button');
+let nextButton = document.getElementById('next-button');
+let ctx = document.getElementById('myChart');
+var percentageChart = document.getElementById('myChart2');
+var lineChart = document.getElementById('myChart3');
+var radarChart = document.getElementById('myChart4');
+var polarAreaChart = document.getElementById('myChart5');
 
-let round=25;
+
+
+let arrOfName=[];
+let round=10;
 let imageNumber=0;
+let votes2=[];
+let shows2=[];
+var graphIndex = 0;
+
+prevButton.addEventListener('click', prevGraph);
+nextButton.addEventListener('click', nextGraph);
+
+      
+      function switchGraph() {
+    switch(graphIndex){
+    case 0:
+       ctx.style.visibility = 'visible';
+      ctx.style.height = '530px';
+      percentageChart.style.visibility = 'hidden';
+     percentageChart.height="0px";
+    lineChart.style.visibility = 'hidden';
+      lineChart.style.height = '0px';
+    radarChart.style.visibility = 'hidden';
+      radarChart.style.height = '0px';
+      polarAreaChart.style.visibility = 'hidden';
+      polarAreaChart.style.height = '0px';
+      break;
+    case 1:
+        document.getElementById('myChart').style.visibility = 'hidden';
+        document.getElementById('myChart').style.height = '0px';
+        document.getElementById('myChart2').style.visibility = 'hidden';
+        document.getElementById('myChart2').style.height = '0px';
+        document.getElementById('myChart3').style.visibility = 'visible';
+        document.getElementById('myChart3').style.height = '530px';
+        document.getElementById('myChart4').style.visibility = 'hidden';
+        document.getElementById('myChart4').style.height = '0px';
+        document.getElementById('myChart5').style.visibility = 'hidden';
+        document.getElementById('myChart5').style.height = '0px';
+        
+      break;
+    case 2:
+        document.getElementById('myChart').style.visibility = 'hidden';
+        document.getElementById('myChart').style.height = '0px';
+        document.getElementById('myChart2').style.visibility = 'visible';
+        document.getElementById('myChart2').style.height = '530px';
+        document.getElementById('myChart3').style.visibility = 'hidden';
+        document.getElementById('myChart3').style.height = '0px';
+        document.getElementById('myChart4').style.visibility = 'hidden';
+        document.getElementById('myChart4').style.height = '0px';
+        document.getElementById('myChart5').style.visibility = 'hidden';
+        document.getElementById('myChart5').style.height = '0px';
+       
+      break;
+         case 3:
+        document.getElementById('myChart').style.visibility = 'hidden';
+        document.getElementById('myChart').style.height = '0px';
+        document.getElementById('myChart2').style.visibility = 'hidden';
+        document.getElementById('myChart2').style.height = '0px';
+        document.getElementById('myChart3').style.visibility = 'hidden';
+        document.getElementById('myChart3').style.height = '0px';
+        document.getElementById('myChart4').style.visibility = 'visible';
+        document.getElementById('myChart4').style.height = '530px';
+        document.getElementById('myChart5').style.visibility = 'hidden';
+        document.getElementById('myChart5').style.height = '0px';
+        
+      break;
+      case 4:
+        document.getElementById('myChart').style.visibility = 'hidden';
+        document.getElementById('myChart').style.height = '0px';
+        document.getElementById('myChart2').style.visibility = 'hidden';
+        document.getElementById('myChart2').style.height = '0px';
+        document.getElementById('myChart3').style.visibility = 'hidden';
+        document.getElementById('myChart3').style.height = '0px';
+        document.getElementById('myChart4').style.visibility = 'hidden';
+        document.getElementById('myChart4').style.height = '0px';
+        document.getElementById('myChart5').style.visibility = 'visible';
+        document.getElementById('myChart5').style.height = '530px';
+     
+      break;    
+        
+    default:
+      //do nothing
+    }
+  }
+
+// set graph to previous graph and jump to prev anchor
+function prevGraph() {
+if(graphIndex === 0) {
+graphIndex = 4;
+} else {
+graphIndex--;
+}
+switchGraph();
+location.href = '#prev-button';
+}
+
+// set graph to next graph and jump to next anchor
+function nextGraph() {
+if(graphIndex === 4) {
+graphIndex = 0;
+} else {
+graphIndex++;
+}
+switchGraph();
+location.href = '#next-button';
+}
+
+
 function CategoryImg(name,filePath){
 this.name=name;
 this.filePath=filePath;
 this.shows=0;
 this.votes=0;
+arrOfName.push(name);
 CategoryImg.allImg.push(this);
 }
 let filesArr=
@@ -30,23 +144,39 @@ new CategoryImg(filesArr[x],`img/${filesArr[x]}.jpg`)
 }
 
 let left,right,center=null;
+let prevLeft,prevRight,prevCenter=null;
+let prevArr=[];
 
 function displayImg(){
      left=generateRandomPic();
      right=generateRandomPic();
      center=generateRandomPic();
-    while(left === center || left === right || right===center || right===left || center ===right || center === left){
+   
+    while(left === center || left === right || right===center || right===left || center ===right || center === left
+       || prevArr.includes(CategoryImg.allImg[left].filePath) 
+       || prevArr.includes(CategoryImg.allImg[center].filePath) 
+       || prevArr.includes(CategoryImg.allImg[right].filePath)){
          left=generateRandomPic();
          right=generateRandomPic();
          center=generateRandomPic();
+         
 }
-   
+
+prevLeft=CategoryImg.allImg[left].filePath;
+prevRight=CategoryImg.allImg[right].filePath;
+prevCenter=CategoryImg.allImg[center].filePath;
+
+prevArr=[prevLeft,prevRight,prevCenter];
 leftImgEl.src=CategoryImg.allImg[left].filePath;
 centerImgEl.src=CategoryImg.allImg[center].filePath;
 rightImgEl.src=CategoryImg.allImg[right].filePath;
 CategoryImg.allImg[left].shows++;
 CategoryImg.allImg[center].shows++;
 CategoryImg.allImg[right].shows++;
+
+
+
+//console.log(center," ", left, " ", right);
 
 }
 
@@ -69,7 +199,7 @@ buttonRound.onclick = function() {
     roundNo=document.getElementById('roundNo').value;
     console.log(roundNo)
     if(roundNo==null || roundNo ==0){
-        round=25;
+        round=3;
     }
     else{
     round=roundNo;
@@ -94,19 +224,23 @@ pressCount+=1;
     else{
         return;
     }
+
     generateRandomPic();
     displayImg();
    
     console.log(CategoryImg.allImg);
 }
-else{
+if(pressCount === round- 1){
     submitButton.onclick = function() { 
         submitButton.disabled = false;
         displayResult();
+        document.getElementById('select-graph').style.visibility = 'visible';
+        gettingChart();
         submitButton.disabled = true;
 
     
     };
+   
 /*     leftImgEl.removeEventListener('click',imageHandler);
 centerImgEl.removeEventListener('click',imageHandler);
 rightImgEl.removeEventListener('click',imageHandler); */
@@ -122,9 +256,12 @@ function displayResult(){
     parrentEl.appendChild(ulEl);
     
     for(var i=0;i<CategoryImg.allImg.length;i++){
+        votes2.push(CategoryImg.allImg[i].votes);
+        shows2.push(CategoryImg.allImg[i].shows);
     var liEl=document.createElement('li');
     liEl.textContent=`${CategoryImg.allImg[i].name} had ${CategoryImg.allImg[i].votes} votes, and was seen ${CategoryImg.allImg[i].shows} times.`;
     ulEl.appendChild(liEl);
+    
 }
 
 }
@@ -139,9 +276,146 @@ function getSelectValue()
             }
         }
 
-        getSelectValue();
-        console.log(imageNumber);
+        //getSelectValue();
+        //console.log(imageNumber);
+        
 
-displayImg();
+        function gettingChart(){
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: arrOfName,
+                    datasets: [{
+                        label: '# of Votes',
+                        data: votes2,
+                        backgroundColor: 
+                            'rgba(255, c, 132, 0.4)',
+
+                        
+                    },{
+                      label: '# of Seen',
+                      data: shows2,
+                      backgroundColor:
+                        'rgba(200, 120, 132, 0.5)',
+                        
+                      
+                  }
+                  ]
+                }
+            });
+            new Chart(percentageChart.getContext('2d'),{
+                type: 'bar',
+                data: {
+                  labels:arrOfName,
+                  datasets: [{
+                    label: '% of Times Picked',
+                    data: votes2,
+                    backgroundColor: 'green',
+                  }, {
+                    label: '# of Seen',
+                    data: shows2,
+                    backgroundColor:
+                      'rgba(200, 120, 132, 0.5)',
+                         
+                }
+                
+                ],
+                },
+                
+               
+              });
+
+              new Chart(percentageChart, {
+                type: 'pie',
+                data: {
+                  labels: arrOfName,
+                  datasets: [
+                    {
+                      backgroundColor: 'yellow',
+                      data: votes2,
+                    },{
+                        label: '# of Seen',
+                        data: shows2,
+                        backgroundColor:
+                          'rgba(200, 120, 132, 0.5)',
+                             
+                    }
+                  ],
 
 
+                },
+               
+              });
+              new Chart(radarChart, {
+                type: 'radar',
+                data: {
+                  labels: arrOfName,
+                  datasets: [
+                    {
+                      backgroundColor: 'yellow',
+                      data: votes2,
+                    },{
+                        label: '# of Seen',
+                        data: shows2,
+                        backgroundColor:
+                          'rgba(200, 120, 132, 0.5)',
+                             
+                    }
+                  ],
+
+
+                },
+               
+              });
+              new Chart(lineChart, {
+                type: 'line',
+                data: {
+                  labels: arrOfName,
+                  datasets: [
+                    {
+                      backgroundColor: 'yellow',
+                      data: votes2,
+                    },{
+                        label: '# of Seen',
+                        data: shows2,
+                        backgroundColor:
+                          'rgba(200, 120, 132, 0.5)',
+                             
+                    }
+                  ],
+
+
+                },
+               
+              });
+              new Chart(polarAreaChart, {
+                type: 'polarArea',
+                data: {
+                  labels: arrOfName,
+                  datasets: [
+                    {
+                      backgroundColor: 'yellow',
+                      data: votes2,
+                    },{
+                        label: '# of Seen',
+                        data: shows2,
+                        backgroundColor:
+                          'rgba(200, 120, 132, 0.5)',
+                             
+                    }
+                  ],
+
+
+                },
+               
+              });
+              
+          
+            
+            console.log(shows2);
+            console.log(votes2);
+        }
+
+
+
+       
